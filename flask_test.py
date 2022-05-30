37e75d3fc98412138a51bc22beb607b256c848bb
sr98877 <srronak11@gmail.com>
2022-05-30 18:02:24 +0530

    flask app v1

---
+++ flask_test.py
@@ -0,0 +1,27 @@
+from flask import Flask,jsonify
+
+app = Flask(__name__)
+
+
+tasks = {
+    {
+        "id":1,
+        "title": u'buy groceries',
+        "description": U'Milk,cheese,bread,butter',
+        "done": False
+     },
+    {
+        "id":2,
+        "title": u'Rentting',
+        "description": U'We are renting ec2 server from aws cloud',
+        "done": False
+    }
+}
+
+@app.route("/", methods=["POST", "GET"])
+
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+if __name__ == "__main__":
+    app.run(debug=True)
